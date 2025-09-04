19cfb86d930b7541309b31a6b767f15d30b6da8d
Kentangz <adhitsholawat12@gmail.com>
2025-09-04 12:14:19 +0700

    change route web to fe

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () { return view('welcome'); });
+// Route::get('/', function () { return view('welcome'); });
 
-// Route::get('/{any}', function () { return file_get_contents(public_path('index.html')); })->where('any', '^(?!api).*$');
+Route::get('/{any}', function () { return file_get_contents(public_path('index.html')); })->where('any', '^(?!api).*$');
 
 Route::get('/reset-password/{token}', function (string $token) {
     return redirect(config('app.frontend_url') . '/reset-password?token=' . $token . '&email=' . request('email'));
